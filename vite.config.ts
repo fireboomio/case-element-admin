541b8599d50420433a644df379acfa4ea3f9c0b7
erguotou <erguotou525@gmail.com>
2023-04-07 14:54:50 +0800

    fix: 不自动open

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     server: {
       host: '0.0.0.0',
       port: Number(env.VITE_APP_PORT),
-      open: true, // 运行是否自动打开浏览器
+      open: false, // 运行是否自动打开浏览器
       // 反向代理解决跨域
       proxy: {
         '/operations': {
